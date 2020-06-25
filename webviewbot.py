508fc362178df863401fe2c910e8c0ec180cffde
royal-tech <55936553+royal-tech@users.noreply.github.com>
2020-06-25 18:27:45 +0530

    webviewbot.py

--- webviewbot.py
+++ webviewbot.py
@@ -47,7 +47,7 @@ def referer_list():
 	global headers_referers
 	headers_referers.append('http://www.google.com/?q=')
 	headers_referers.append('http://www.usatoday.com/search/results?q=')
-	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://www.google.com/?q=')
 	headers_referers.append('http://' + host + '/')
 	return(headers_referers)
 	
